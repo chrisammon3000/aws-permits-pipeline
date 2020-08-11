ff645da9e223b5352bbaabc25c61310237b5a439
Gregory Lindsey <gclindsey@gmail.com>
2020-08-11 12:48:14 -0700

    Update layers serverless.yml

--- src/functions/ingest/transform_raw_data.py
+++ src/functions/ingest/transform_raw_data.py
@@ -1,8 +1,7 @@
 import os
 import logging
 import boto3
-import json
-from events.s3_trigger import s3_trigger
+import pandas as pd
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -12,35 +11,17 @@ S3_BUCKET = os.environ['S3_BUCKET']
 
 s3 = boto3.client('s3')
 
-
 def transform_raw_data(event, context):
 
     logger.info(f"Event: {event}")
 
     for record in event['Records']:
         key = record['s3']['object']['key']
-        logger.info(f"Key: {key}")
-        response = s3.get_object(Bucket=S3_BUCKET, Key=key)
-        logger.info(f"Response: {response}")
-        content = response['Body']
-        jsonObject = json.loads(content.read())
-        logger.info(f"Content: {jsonObject[:100]}")
+        logger.info(f"Object Key: {key}")
 
+        dataframe = pd.read_csv("s3://" + S3_BUCKET + "/" + key)
+        logger.info(dataframe.head())
 
-    # # Convert to for loop
-    # for record in event['Records']:
-
-    #     key = record['s3']['object']['key']
-
-    #     logger.info("Key:\n", key)
-
-        #response = s3.get_object(Bucket=S3_BUCKET, Key=key)
-
-        #logger.info("Response:\n", response)
-
-    # content = response['Body']
-
-    # jsonObject = json.loads(content.read)
         
     return {
         "message": "SUCCESS"
